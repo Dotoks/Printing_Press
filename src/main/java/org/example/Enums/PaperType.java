0501c4cd102a0e008fb8b39c3cc731a10b65cc03
Ivo Stefanov Handzhiyski <ivo.handzhiyski@blankfactor.com>
2023-05-16 10:33:46 +0300

    finished paperType

--- src/main/java/org/example/Enums/PaperType.java
+++ src/main/java/org/example/Enums/PaperType.java
@@ -15,14 +15,32 @@ public enum PaperType {
     }
 
     private double GetNormalPrice(PageSize pageSize){
-        return 0;
+        return switch (pageSize){
+            case A1 -> 1;
+            case A2 -> 0.75;
+            case A3 -> 0.5;
+            case A4 -> 0.35;
+            case A5 -> 0.20;
+        };
     }
 
     private double getGlossyPrice(PageSize pageSize){
-        return 0;
+        return switch (pageSize){
+            case A1 -> 1.15;
+            case A2 -> 0.90;
+            case A3 -> 0.65;
+            case A4 -> 0.50;
+            case A5 -> 0.35;
+        };
     }
 
     private double getNewsPaperPrice(PageSize pageSize){
-        return 0;
+        return switch (pageSize){
+            case A1 -> 0.85;
+            case A2 -> 0.60;
+            case A3 -> 0.35;
+            case A4 -> 0.20;
+            case A5 -> 0.05;
+        };
     }
 }
